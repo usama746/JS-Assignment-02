9d81dc03e8d25360ba7474e384181d85e95f3314
Usama Khan <usama608474@gmail.com>
2024-12-22 21:40:36 +0500

    files

---
+++ app.js
@@ -0,0 +1,62 @@
+// QUESTION 01 & 02
+
+var username = "usama746";
+var myName = "Usama Khan";
+
+
+
+
+// QUESTION 03
+var message;
+message = "Hello World";
+alert(message);
+
+
+
+
+
+// QUESTION 04
+
+var name = "Usama Khan";
+var age = "27 Years Old";
+var course = "SMIT-Modern Web Application"
+
+
+alert(name)
+alert(age)
+alert(course)
+
+
+
+
+// QUESTION 05
+
+var pizza = ("PIZZA \nPIZZ \nPIZ \nPI \nP");
+alert(pizza);
+
+
+
+
+// QUESTION 06
+
+var email = "usama608474@gmail.com";
+alert("My email address is " + email);
+
+
+
+
+// QUESTION 07
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying yo learn from the book called " + book);
+
+
+
+
+
+
+// QUESTION 08
+
+var para = "Yah! I can write HTML content through JavaScript";
+document.write(para);
+
